074b73cadc81bf2a45f04c196aa29cb31c2e9253
victory2 <victory@example.com>
2017-09-05 07:47:00 +0800

    cc

--- user/js/wx.js
+++ user/js/wx.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
 	wx.ready(function(){
+		wx.onMenuShareQQ({
+			title: '我爱你', // 分享标题
+			desc: '哦，这样啊', // 分享描述
+			link: 'www.baidu.com', // 分享链接
+			imgUrl: '', // 分享图标
+			success: function () { 
+			   console.log("success");
+			},
+			cancel: function () { 
+			   console.log("fail");
+			}
+		});
 		
-		alert("成功");
 	});
 });
