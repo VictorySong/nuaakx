50d38e15a1f2aff9e30ec0073c2102fe7be92b86
Your Name <email@example.com>
2017-10-07 23:10:47 +0800

    haha

--- user/index.php
+++ user/index.php
@@ -693,12 +693,12 @@ $signPackage = $jssdk->GetSignPackage();
 				  <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
 				  <h2>Heading</h2>
 				  <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-				  <p><a class="btn btn-default" href="#" role="button">
 						<label>
-							<input type="checkbox" name="department" value="KxXqChy" >创意设计部
-						</label>
-				  View details &raquo;</a></p>
+				  <label><div class="btn btn-default" href="#" role="button">
+					<input type="checkbox" name="department" value="KxXqChy" >创意设计部
+				  </div></label>
 				</div>
+				
 				<div class="col-xs-6">
 				  <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
 				  <h2>Heading</h2>
