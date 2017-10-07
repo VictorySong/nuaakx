36ad53be1c4b4ebfd1ccdb1d3ea478c5f4568061
Your Name <email@example.com>
2017-10-07 23:15:07 +0800

    haha

--- user/index.php
+++ user/index.php
@@ -680,7 +680,7 @@ $signPackage = $jssdk->GetSignPackage();
 		<!-- ............................................    -->
 		
 		<!--  活动投票    -->
-		<div id="vote" style="padding-left:15px; padding-right:15px; padding-bottom:30px; display:none;" cont="content">
+		<div id="vote" style="padding-left:25px; padding-right:15px; padding-bottom:30px; display:none;" cont="content">
 			<div style="width:100%;" cont="head">
 			</div>
 		
@@ -705,6 +705,10 @@ $signPackage = $jssdk->GetSignPackage();
 				  <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
 				  <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
 				</div>
+				
+				<hr class="featurette-divider">
+				
+				
 				<div class="col-xs-6">
 				  <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
 				  <h2>Heading</h2>
