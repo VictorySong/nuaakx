2f66bfb600af38d919b3da4bb78c1da303ee2cd4
Your Name <email@example.com>
2017-10-09 00:16:56 +0800

    hh

--- user/index.php
+++ user/index.php
@@ -786,7 +786,7 @@ $signPackage = $jssdk->GetSignPackage();
 				  <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
 				  <h2>Heading</h2>
 				  <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-				  <p><a class="btn btn-default" href="#" role="button"><input type="checkbox" name="department" value="KxXqChy" ><i>1.</i>Heading</a></p>
+				  <p><a class="btn btn-default" href="#" role="button"><input type="checkbox" name="provote" value="vote1" ><i>1.</i>Heading</a></p>
 				</div>
 				
 				<div class="col-xs-6" style="padding-bottom:15px;">
---
+++ vote.php
@@ -0,0 +1,4 @@
+<?php
+	echo $_POST["provote"]; 
+	echo "haha";
+?>
