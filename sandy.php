c272e2de131a17755e5bd388c209172eba927712
Zhaoshenjia <hehua88656@163.com>
2017-10-16 20:55:27 +0800

    hah

--- sandy.php
+++ sandy.php
@@ -7,7 +7,7 @@
     define("TOKEN", "zsjsandy");  
     $wechatObj = new wechatCallbackapiTest();  
     $wechatObj->valid();  
-    //$wechatObj->responseMsg();  
+    $wechatObj->responseMsg();  
       
     class wechatCallbackapiTest  
     {   
