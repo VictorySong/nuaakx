2a6f8565fbd042e90d05687411fcb83528942b31
hyz <3042652167@qq.com>
2017-10-21 20:24:18 +0800

    1

--- hyzphp.php
+++ hyzphp.php
@@ -3,9 +3,10 @@
     $timestamp=$_GET['timestatmp'];
 	$nonce=$_GET['nonce'];
 	$echostr=$_GET['echostr'];
-	 define('TOKEN','1021');
+	define('TOKEN','1021');
 	$tmpArr=array(TOKEN,$timestamp,$nonce);
 	sort($tmpArr,SORT_STRING);
-      
+	$tmpStr=join($tmpArr)
+    $tmpStr=sha1($tmpStr)  
       
 ?>  
