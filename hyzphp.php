cc0465b2f7a19411bbe39fc2879ed71f935f142e
hyz <3042652167@qq.com>
2017-10-21 20:19:29 +0800

    add hyz

---
+++ hyzphp.php
@@ -0,0 +1,11 @@
+<?php  
+    $signature=$_GET['signature'];
+    $timestamp=$_GET['timestatmp'];
+	$nonce=$_GET['nonce'];
+	$echostr=$_GET['echostr'];
+	 define('TOKEN','1021');
+	$tmpArr=array(TOKEN,$timestamp,$nonce);
+	sort($tmpArr,SORT_STRING);
+      
+      
+?>  
