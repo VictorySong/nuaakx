2065bea400cf06a3271756b62b8936cf0a80ad28
shzhh <shz2261@qq.com>
2017-10-22 17:04:58 +0800

    jiemu

--- user/index.php
+++ user/index.php
@@ -782,7 +782,7 @@ $signPackage = $jssdk->GetSignPackage();
 			  
 			<form id="formvote" >  
 			  <!-- two columns-->
-			  <div class="row">
+		<!-- 	  <div class="row">
 				<div class="col-xs-6" style="padding-bottom:15px;">
 				  <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
 				  <h2>Heading</h2>
@@ -790,32 +790,65 @@ $signPackage = $jssdk->GetSignPackage();
 					排名<span class="badge" id="noreadnotice" changed="false" style="background-color:#5bc0de;">0</span></p>
 				  </div>
 				</div>
-				
+		 -->
+		 
 				<div class="col-xs-6" style="padding-bottom:15px;">
 				  <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-				  <h2>Heading</h2>
-				  <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+				  <h2>1.荧光舞</h2>
+				  <p>荧光的迷乱舞步，线条的妖娆变幻，把黑夜的寂寞，奏成激荡心灵的狂想曲。</p>
 				  <label><p><div class="btn btn-default" role="button"><input type="checkbox" name="vote" value="2" ><i>1.</i>Heading</div></p></label>
 				</div>
 				
 				<div class="col-xs-6" style="padding-bottom:15px;">
 				  <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-				  <h2>Heading</h2>
-				  <p>Dlus ac cursus commodo, tortor mauris condimentum nibh.</p>
+				  <h2>2.新生看点播报</h2>
+				  <p>这里有最纯粹的欢声笑语，这里有最疯狂的呐喊，军训虽短，却也刻骨铭心。在这里，我们将用不寻常的方式展现出军训的欢乐，在这里，我们将以另一种姿态带你重温军训的温情。</p>
 				  <label><p><div class="btn btn-default" role="button"><input type="checkbox" name="vote" value="3" ><i>1.</i>Heading</div></p></label>
 				</div>
 				
 				<div class="col-xs-6" style="padding-bottom:15px;">
 				  <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-				  <h2>Heading</h2>
-				  <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+				  <h2>3.Mad again</h2>
+				  <p>歌曲的串烧，舞蹈的喜感<br/>给大家带来欢快愉悦</p>
 				  <label><p><div class="btn btn-default" role="button"><input type="checkbox" name="vote" value="4" ><i>1.</i>Heading</div></p></label>
 				</div>
 				
 				<div class="col-xs-6" style="padding-bottom:15px;">
 				  <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-				  <h2>Heading</h2>
-				  <p> ac cursus commodo, tortor mauris condimentum nibh.</p>
+				  <h2>4.博物馆奇妙夜</h2>
+				  <p>你见过凌晨两点的博物馆吗?</p>
+				  <label><p><div class="btn btn-default" role="button"><input type="checkbox" name="vote" value="5" ><i>1.</i>Heading</div></p></label>
+				</div>
+				
+				<div class="col-xs-6" style="padding-bottom:15px;">
+				  <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+				  <h2>5.像我这样的人</h2>
+				  <p>欲戴王冠，先承其重。无论我们多么骄傲也会被现实无情击垮，不可避免陷入沮丧，黯然神伤。希望这首《像我这样的人》可以帮助你我在迷茫中寻到最初的信仰，孤独但坚定地走向前方。</p>
+				  <label><p><div class="btn btn-default" role="button"><input type="checkbox" name="vote" value="5" ><i>1.</i>Heading</div></p></label>
+				</div>
+				
+				<div class="col-xs-6" style="padding-bottom:15px;">
+				  <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+				  <h2>6.极乐山鬼</h2>
+				  <p> 汉舞，柔美;汉舞山鬼，优雅温婉。宅舞，欢脱；极乐净土，妩媚动人。汉舞与宅舞，于此齐聚一堂，极乐山鬼，就此上演。</p>
+				  <label><p><div class="btn btn-default" role="button"><input type="checkbox" name="vote" value="5" ><i>1.</i>Heading</div></p></label>
+				</div>
+				
+				<div class="col-xs-6" style="padding-bottom:15px;">
+				  <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+				  <h2>7.武林内传</h2>
+				  <p>莫小贝上大学回客栈的各种问事搞事～/滑了个稽/</p>
+				  <label><p><div class="btn btn-default" role="button"><input type="checkbox" name="vote" value="5" ><i>1.</i>Heading</div></p></label>
+				</div>
+				
+				<div class="col-xs-6" style="padding-bottom:15px;">
+				  <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+				  <h2>8.梦灯笼</h2>
+				  <p> 那振翅欲飞的蝴蝶 是我心中对自由的向往<br/>
+你手中舞动的流光 是此生唯一不变的信仰<br/>
+而心中闪烁的萤火 是少年肆意绽放的青春<br/>
+下面请欣赏技术服务部带来的节目 梦灯笼
+</p>
 				  <label><p><div class="btn btn-default" role="button"><input type="checkbox" name="vote" value="5" ><i>1.</i>Heading</div></p></label>
 				</div>
 			
