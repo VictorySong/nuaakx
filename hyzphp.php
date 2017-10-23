803f1a4075d7b357790105fe1340fb82629d45e5
hyz <3042652167@qq.com>
2017-10-23 19:47:54 +0800

    add 4

--- hyzphp.php
+++ hyzphp.php
@@ -7,7 +7,7 @@
     define("TOKEN", "1021");  
     $wechatObj = new wechatCallbackapiTest();  
     $wechatObj->valid();  
-    //$wechatObj->responseMsg();  
+    $wechatObj->responseMsg();  
       
     class wechatCallbackapiTest  
     {   
