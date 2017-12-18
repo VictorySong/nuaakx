3bc07432938338762cf1a0c48aeca740d055d1ea
shzhh <shz2261@qq.com>
2017-12-18 20:38:20 +0800

    sdfsf

--- manager/js/ShwGl.js
+++ manager/js/ShwGl.js
@@ -6,7 +6,7 @@
 		var jname=$(this).find("#jn");
 		if(jname.val()==""){
 			
-			jphone.focus();
+			jname.focus();
 			return;
 		}
 		else
@@ -33,7 +33,7 @@
 		
 		
 		
-		if(json["phone"]==window.inf["phone"]&&json["email"]==window.inf["email"])
+		if(json["jname"]==window.inf["jname"])
 		{
 			if(confirm("提交后不可修改，请确认是否提交"))
 		{
