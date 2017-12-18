18128e848feabdf45e8d02dde0fc8f4e3fd0b467
hahaha <10075432432@qq.com>
2017-12-18 09:43:16 +0800

    peixun1

--- peixun/bianliang.php
+++ peixun/bianliang.php
@@ -16,18 +16,17 @@ $x=5; // global scope
   
 function myTest() {
    $y=10; // local scope
-   echo "<p>在函数内部测试变量：</p>";
-   echo "变量 x 是：$x";
+
+   echo "x is $x";
    echo "<br>";
-   echo "变量 y 是：$y";
+   echo "y is $y";
 } 
 
 myTest();
 
-echo "<p>在函数之外测试变量：</p>";
-echo "变量 x 是：$x";
+echo "x is $x";
 echo "<br>";
-echo "变量 y 是：$y";
+echo "y is $y";
 ?>
 
 </body>
