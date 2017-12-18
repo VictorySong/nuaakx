8c3b23c677427e131c28f7dacfbf4e5438c893b6
dada <957754840@qq.com>
2017-12-18 22:48:02 +0800

    text1

---
+++ sjh/biaodan.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+
+<html> 
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+
+<form action="biaodan2" method="post">
+学号：<input type="text" name="number"><br>
+姓名：<input type="text" name="name"><br>
+性别:<br>
+<input type="radio" name="gender" value="male" checked />male<br>
+<input type="radio" name="gender" value="female"/>female<br>
+单身与否:<br>
+<input type="radio" name="single" value="yes" checked />yes<br>
+<input type="radio" name="single" value="no"/>no<br>
+年龄:<input type="text" name="age"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ sjh/compare.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+$s=array(1,6,4,5,9);
+sort($s);
+for($x=0;$x<count($s);$x++)
+{
+	echo $s[$x];
+}
+?>  
+
+</body>
+</html>
