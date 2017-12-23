0ec36169b3eba32dada4c165a3b554654ec0fa39
hyz <3042652167@qq.com>
2017-12-23 11:27:03 +0800

    sdf

---
+++ hyz/wn.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+ <head>
+            <meta charset="UTF-8">
+   
+    </head>
+<body>
+
+<?php
+$t=date("H");
+
+
+
+if ($t<"8") 
+ {
+   echo "睡什么起来(｡･∀･)ﾉﾞ嗨";
+ } 
+else
+ {
+     echo "晚安!";
+ }
+
+
+
+?>  
+</body>
+</html>
---
+++ hyz/zfc.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>hahhahahah!</h1>
+
+<?php
+$sz=array("V","BMW","SAAB","as","we"); 
+$i=0;
+$length=count($sz);
+while($i<$length)
+ {
+     echo $sz[$i];
+     echo "<br>"
+     $i++;
+ }
+ $a=""
+ for($i=0;$i<5;$i++)
+  {
+      $a=$a.$sz[$i];
+  }
+  $b=$a;
+  echo $b;
+?>  
+
+</body>
+</html>
