4bd6866e9b22a64f7e332a31cfeec24ca81b20f3
luominghao <1872488085@qq.com>
2017-12-24 13:47:27 +0800

    php

---
+++ luominghao/geren.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE HTML>
+
+<html> 
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+
+<form action="fh.php" method="post">
+学号：<input type="text" name="xh"><br>
+姓名：<input type="text" name="xm"><br>
+性别：<input type="text" name="xb"><br>
+是否单身：<input type="text" name="sfds"><br>
+年龄：<input type="text" name="nl"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
