800a77e2a2591507fbc202d6292930253344f95c
likstar <2106509629@qq.com>
2017-12-25 18:44:06 +0800

    ?

---
+++ wx/wx/one.php
@@ -0,0 +1,11 @@
+<html>
+<meta charset="utf-8">
+<body>
+
+xuehao :<?php echo $_POST["xh"]; ?><br>
+name : <?php echo $_POST["xm"]; ?><br>
+sex :  <?php echo $_POST["xb"]; ?><br>
+是否单身 :  <?php echo $_POST["sfds"]; ?><br>
+age :  <?php echo $_POST["nl"]; ?><br>
+</body>
+</html>
