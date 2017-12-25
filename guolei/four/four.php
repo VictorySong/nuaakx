cdee6479a391b3defa6a74e800c41b8ac9602d18
guo lei <1021556596@qq.com>
2017-12-25 18:26:42 +0800

    gl

---
+++ guolei/four/four.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+    学号：<?php echo $_POST["1"]; ?><br>
+    姓名：<?php echo $_POST["2"]; ?><br>
+    性别：<?php echo $_POST["3"]; ?><br>
+    是否单身：<?php echo $_POST["4"]; ?><br>
+    年龄：<?php echo $_POST["name"]; ?><br>
+</body>
+</html>
---
+++ guolei/one.php
@@ -0,0 +1,9 @@
+<?php
+$we=array("1","2","3","4","5","6");
+$arrlength=count($we);
+for($x=o;$x<$arrlength;$x++){
+    echo $we[$x];
+    echo "<br>";
+}
+$you=we[0].we[1].we[2].we[3].we[4].we[5]
+?>
