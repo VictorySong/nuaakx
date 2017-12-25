14bb4429f15969c69d2c4db847c883ada8f6d98c
shzhh <shz2261@qq.com>
2017-12-25 15:45:47 +0800

    sdfsd

--- manager/js/ShwGl.js
+++ manager/js/ShwGl.js
@@ -38,7 +38,7 @@
 		json["jdate"]=jdate.val();
 		
 		var jtime=$(this).find("input[name='jtime']");
-		json["jtime"]=jtime.val();
+		json["jtime"]=jtime;
 		
 		var r=confirm("提交后不可修改，请确认是否提交");
 		if (r==true)
