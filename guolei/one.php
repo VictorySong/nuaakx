740569f0f4e42656d10f1337f1a5df89d450bfb3
guo lei <1021556596@qq.com>
2017-12-25 19:01:57 +0800

    gl

--- guolei/one.php
+++ guolei/one.php
@@ -1,11 +1,19 @@
 <?php
 $we=array("1","2","3","4","5","6");
 $arrlength=count($we);
-$x=0;
-for($x=o;$x<$arrlength;$x++){
-    echo $we[$x];
+    echo $we[0];
     echo "<br>";
-}
+	 echo $we[1];
+    echo "<br>";
+	 echo $we[2];
+    echo "<br>";
+	 echo $we[3];
+    echo "<br>";
+	 echo $we[4];
+    echo "<br>";
+	 echo $we[5];
+    echo "<br>";
+
 $you=$we[0].$we[1].$we[2].$we[3].$we[4].$we[5];
 echo $you;
 ?>
