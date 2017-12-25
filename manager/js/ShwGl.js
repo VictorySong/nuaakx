014f9a34d6f73d11621485fefba034881cb974a2
shzhh <shz2261@qq.com>
2017-12-25 16:05:24 +0800

    sdfsd

--- manager/js/ShwGl.js
+++ manager/js/ShwGl.js
@@ -38,6 +38,7 @@
 		json["jdate"]=jdate.val();
 		
 		var jtime=$(this).find("input[name='jtime']");
+		json["department"]=[];
 		jtime.each(function(){
 				json["jtime"][json["jtime"].length]=$(this).val();
 			});
