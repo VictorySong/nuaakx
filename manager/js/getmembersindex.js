23cdb1caee65ee3ec363cf204ed461444ed3beaa
shzhh <shz2261@qq.com>
2017-12-26 17:36:38 +0800

    sdfds

--- manager/js/getmembersindex.js
+++ manager/js/getmembersindex.js
@@ -180,7 +180,7 @@ function getkxts(){
 	
 	$.post("getkxts.php",{p:window.recruitp,tableName:window.tableName}).done(function(data){
 		console.log(data);
-		$("#kxts").append('<div id="nomore" style="text-align:center;">无更多</div>');
+		$("a").append('<div id="nomore" style="text-align:center;">无更多</div>');
 		try{
 			var da=JSON.parse(data);
 		}catch(e){
