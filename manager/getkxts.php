38c9c1877cdd804553f9eeb0d84ec712899860ee
hahaha <10075432432@qq.com>
2017-12-26 20:01:49 +0800

    a

--- manager/getkxts.php
+++ manager/getkxts.php
@@ -3,16 +3,14 @@ include("../SaeMysql.php");
 //session_set_cookie_params(7200,'/','nuaakx.com');
 session_start();
 define("N",20);
-if(!empty($_SESSION["tableName"]) )
-{
+ 
+
 	$mysql=new SaeMysql();	
 	$sql="SELECT * FROM `KxTs`";
 	$result1=mysql_query($sql,$mysql->ico);
 	$num=mysql_num_rows($result1);
 	$result=array();//用来存放结果
 	$k=0;//用来计数
-	if($t)
-	{
 		for($i=0;$i<$num; $i++)
 		{
 			
@@ -26,10 +24,10 @@ if(!empty($_SESSION["tableName"]) )
 				$k++;
 			
 		}
-	}
+	
 	$json["error"]=0;
 	$json["msg"]=$result;
 	$json["num"]=$num;
 	echo json_encode($json);
-}
+
 ?>
--- manager/js/ShwGl.js
+++ manager/js/ShwGl.js
@@ -65,3 +65,80 @@
 		
 			
 	});
+	
+	
+	
+	
+	function getwzjy(){
+	$("#fixdate").text(window.fixcomputerdate);
+	$("#itemlog1").children().remove();
+	console.log(window.fixcomputerdate);
+	$.post("getfixcomputer.php",{date:window.fixcomputerdate,date1:window.fixcomputerdate1}).done(function(data){
+		console.log(data);
+			
+		try{
+			var da=JSON.parse(data);
+		}catch(e){
+			console.log(e);
+			return;
+		}
+			if(da["error"]==0)
+			{
+				
+				for(var p in da["msg"])
+				{
+					var html='<div class="panel panel-default" >\
+								<div class="panel-heading">\
+									<h3 class="panel-title" cont="name"  role="button" data-toggle="collapse" data-parent="#recruit1" data-target="#'+da["msg"][p]["stId"]+'">\
+										'+da["msg"][p]["stId"]+'\
+										&nbsp;&nbsp;&nbsp; '+da["msg"][p]["name"]+'&nbsp;&nbsp;&nbsp; '+da["msg"][p]["place"]+'\
+									</h3>\
+								</div>\
+								<div id="'+da["msg"][p]["stId"]+'" class="panel-collapse collapse">\
+									<div class="panel-body">\
+										<div class="form-group">\
+											<label >学号:</label>\
+											<input type="text" class="form-control" disabled value="'+da["msg"][p]["stId"]+'" cont="stId">\
+										</div>\
+										<div class="form-group">\
+											<label >姓名:</label>\
+											<input type="text" class="form-control" disabled value="'+da["msg"][p]["name"]+'" cont="name">\
+										</div>\
+										<div class="form-group">\
+											<label >手机:</label>\
+											<input type="text" class="form-control" disabled value="'+da["msg"][p]["phone"]+'" cont="phone">\
+										</div>\
+										<div class="form-group">\
+											<label >邮箱:</label>\
+											<input type="text" class="form-control" disabled value="'+da["msg"][p]["email"]+'" cont="email">\
+										</div>\
+										<div class="form-group">\
+											<label >问题描述:</label>\
+											<div class=""  style="padding-left:6px; padding-right:6px; padding-top:5px; padding-bottom:5px; border-radius:4px; min-height:71px; overflow:scroll; border:1px solid #ccc;" cont="problem">'+(da["msg"][p]["problem"]==null?"":da["msg"][p]["problem"])+'</textarea>\
+										</div>\
+										<div class="form-group">\
+											<label >评价:</label>\
+											<div class="" style="padding-left:6px; padding-right:6px; padding-top:5px; padding-bottom:5px; border-radius:4px; min-height:50px; overflow:scroll; border:1px solid #ccc;" cont="evaluate">'+(da["msg"][p]["evaluate"]==null?"":da["msg"][p]["evaluate"])+'</textarea>\
+										</div>\
+										<div class="form-group">\
+											<label >解决措施:</label>\
+											<div  style="padding-left:6px; padding-right:6px; padding-top:5px; padding-bottom:5px; border-radius:4px; min-height:50px; overflow:scroll; border:1px solid #ccc;"  class="" cont="solution" '+(da["msg"][p]["solution"]==null?"":"already=\"1\"")+' action="insertfixsolution.php" stId="'+da["msg"][p]["stId"]+'">'+(da["msg"][p]["solution"]==null?"":da["msg"][p]["solution"])+'</div>\
+										</div>\
+										<div class="form-group">\
+											<label >回复:</label>\
+											<div  style="padding-left:6px; padding-right:6px; padding-top:5px; padding-bottom:5px; border-radius:4px; min-height:50px; overflow:scroll; border:1px solid #ccc;"  class="" cont="response" '+(da["msg"][p]["response"]==null?"":"already=\"1\"")+' action="insertfixresponse.php" stId="'+da["msg"][p]["stId"]+'">'+(da["msg"][p]["response"]==null?"":da["msg"][p]["response"])+'</div>\
+										</div>';			
+						html+='</div>\
+									</div>\
+								</div>';
+					$("#fixcomputer1").append(html);
+				}
+				$("#fixcomputer1").find("[cont=\"solution\"]").click(fixSR);//解决措施
+				$("#fixcomputer1").find("[cont=\"response\"]").click(fixSR);//回复
+				
+			}
+		
+	});
+	
+	
+}
