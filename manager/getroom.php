31a5973e20da2fb62a951665808abaf8cdb065fd
hahaha <10075432432@qq.com>
2017-12-27 08:21:40 +0800

    peixun1

--- manager/getroom.php
+++ manager/getroom.php
@@ -5,13 +5,14 @@ session_start();
 
 	$mysql=new SaeMysql();
 	$data=$mysql->getData("SELECT `jname`,`jphone`,`date`,`gname`,`description`,`room`,`time` FROM `KxGlwz` WHERE `date`='".$_POST["date"]."' || `date`='".$_POST["date1"]."'");
-	//$data["time1"]=$data["time"]+2;
+	$data["description"]=$data["time"]+2;
 	
 		if(!empty($data))
 		{
 			
 			$json["error"]=0;
 			$json["msg"]=$data;
+			
 			
 			echo json_encode($json);
 		}
