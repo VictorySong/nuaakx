585bf5fedc16bb4c5a627d8a1f3bb44f92fc0e3d
shzhh <shz2261@qq.com>
2018-03-05 16:07:59 +0800

    sfds

--- manager/js/ShwGl.js
+++ manager/js/ShwGl.js
@@ -35,11 +35,13 @@ $(document).ready(function(){
 	var x = 2;	
 	$("#itemtype").text(item[x]);
 	$("#previous").click(function(){
-		x= x-1;	
+		if (x>0){x= x-1;}
+		else{x=3;}
 		$("#itemtype").text(item[x]);
 	});
 	$("#next").click(function(){
-		x= x+1;
+		if (x<3){x= x+1;}
+		else{x=0;}
 		$("#itemtype").text(item[x]);
 	});
 });
