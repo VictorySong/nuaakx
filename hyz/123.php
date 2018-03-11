ffd62fdb0a3e44fc79e37cfc7513a179910e6245
hyz <3042652167@qq.com>
2018-03-11 20:44:16 +0800

    l

--- hyz/123.php
+++ hyz/123.php
@@ -2,7 +2,7 @@
 <meta charset="utf-8">
 <body>
 
-<form method="post" action="<?php echo $_SERVER["php_SELF"]; ?>">
+<form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
   id<input type="text" name="abc">
   <input type="submit">
  </form>
