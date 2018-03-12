b4e056d3f18dbf86c0ff7314da77035a4a854fb2
hahaha <10075432432@qq.com>
2018-03-12 18:15:40 +0800

    a

--- manager/getwzjy.php
+++ manager/getwzjy.php
@@ -4,7 +4,7 @@ include("../SaeMysql.php");
 session_start();
 
 	$mysql=new SaeMysql();
-	$data=$mysql->getData("SELECT `jname`,`jphone`,`jdatetime`,`gname`,`description`,`jdatetime1` FROM `KxGlwz` WHERE `wz`='".$_POST["item"]."'");
+	$data=$mysql->getData("SELECT `jname`,`jphone`,`jdatetime`,`gname`,`description`,`jdatetime1` FROM `KxGlwz` WHERE `wz`='506'");
 	 
 	
 		if(!empty($data))
