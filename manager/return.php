fdd3620417d6f07e7cc575012639a549820acf54
hahaha <10075432432@qq.com>
2018-03-15 16:30:52 +0800

    a

--- manager/return.php
+++ manager/return.php
@@ -4,7 +4,7 @@ include("../SaeMysql.php");
 session_start();
 
 	$mysql=new SaeMysql();
-	if($mysql->runsql("UPDATE `app_nuaakexie`.`KxGlwz2` SET `itemreturn` = '0' WHERE `KxGlwz2`.`jname` = '".$_POST["jname"]."'"))
+	if($mysql->runsql("UPDATE `app_nuaakexie`.`KxGlwz2` SET `itemreturn` = '0' WHERE `KxGlwz2`.`jname` = '".$_POST["jname"]."'&&`gname` = '".$_POST["gname"]."'&&`jdatetime` = '".$_POST["jdatetime"]."'&&`jdatetime1` = '".$_POST["jdatetime1"]."'"))
 				die($mysql->errmsg());
 	$data=$mysql->getData("SELECT `jname`,`jphone`,`jdatetime`,`gname`,`description`,`jdatetime1` FROM `KxGlwz2` WHERE `jname`='".$_POST["jname"]."'");
 	if(!empty($data))
