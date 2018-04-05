4b30dd78fb4b45b31bed2ed9ed88937b3f9109cb
hahaha <10075432432@qq.com>
2018-04-05 15:45:10 +0800

    a

---
+++ manager/getroom.php
@@ -0,0 +1,23 @@
+<?php
+include("../SaeMysql.php");
+//session_set_cookie_params(7200,'/','nuaakx.com');
+session_start();
+
+	$mysql=new SaeMysql();
+	$data=$mysql->getData("SELECT `jname`,`jphone`,`date`,`gname`,`description`,`room`,`time` FROM `KxGlwz` WHERE `date`='".$_POST["date"]."' || `date`='".$_POST["date1"]."'");
+	 
+	
+		if(!empty($data))
+		{
+			
+			$json["error"]=0;
+			$json["msg"]=$data;
+			
+			
+			echo json_encode($json);
+		}
+	
+
+
+
+?>
