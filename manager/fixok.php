2f774fe231ffdd1ea28e4437cc0b0355a396248c
hahaha <10075432432@qq.com>
2018-04-05 15:01:02 +0800

    aa

--- manager/fixok.php
+++ manager/fixok.php
@@ -7,7 +7,7 @@ session_start();
 	$data1=$mysql->getline("SELECT `name`,`phone` FROM `wx_user` WHERE `number`='".$_SESSION["stId"]."'");
 	echo $data1;
 	$name1=$data1["name"];
-	if($mysql->runsql("UPDATE `app_nuaakexie`.`bigfix` SET `ok` = 1,`fixname` = '".$name1."' WHERE `bigfix`.`sname` = '".$_POST["sname"]."'&&`sid` = '".$_POST["sid"]."'&&`tel` = '".$_POST["tel"]."'&&`ques` = '".$_POST["ques"]."'"))
+	if($mysql->runsql("UPDATE `app_nuaakexie`.`bigfix` SET `ok` = 1,`fixname` = '".$name1."' WHERE `bigfix`.`sname` = '".$_POST["sname"]."'&&`sid` = '".$_POST["sid"]."'&&`wx` = '".$_POST["wx"]."'&&`tel` = '".$_POST["tel"]."'&&`ques` = '".$_POST["ques"]."'"))
 				die($mysql->errmsg());
 	$data=$mysql->getData("SELECT `sname`,`sid`,`tel`,`des`,`ques`,`addr` FROM `bigfix` WHERE `sname`='".$_POST["sname"]."'");
 	if(!empty($data))
