e6e60f1d941f9f6955d126e5b0c2a572626fb8f8
hahaha <10075432432@qq.com>
2018-04-05 16:12:47 +0800

    a

--- user/getnotice.php
+++ user/getnotice.php
@@ -5,7 +5,14 @@ session_start();
 if(!empty($_SESSION["stId"]))
 {
 	$mysql=new SaeMysql();
-	$json["notice"]=$mysql->getData("SELECT `noticeid`,`title`,`description`,`url`,`date` FROM `Notice` WHERE `tostId`='".$_SESSION["stId"]."'");
+	$json["notice"]=$mysql->getData("SELECT `noticeid`,`title`,`description`,`url`,`date` FROM `Notice` WHERE `tostId`="'".$_SESSION["stId"]."');
+    foreach($json["notice"] as $key=>$value){
+        $data1=$mysql->getLine("SELECT `num` FROM `ReadNotice` WHERE `noticeid`='".$value["noticeid"]."' && `stId`='".$_SESSION["stId"]."'");
+        if(empty($data1))
+            $json["notice"][$key]["already"]=0;
+        else
+            $json["notice"][$key]["already"]=1;
+    }
 	echo json_encode($json);
 }
 		
