12fdbc20097ee3a55be26ae20444aaa21336dd89
BXY <451812979@qq.com>
2018-05-27 22:58:49 +0800

    111

---
+++ peixun/bxyhhh.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+
+<form action="bxywelcome.php" method="post">
+学号：<input type="number" name="id"><br>
+<input type="submit">
+</form>
+	
+<?php//实现搜索功能。通过网页搜索，输入学号，能在科协数据库查找名字等相关信息并显示出来，通过网页显示
+$json=array(); 
+$_SESSION["stId"]=$_POST['id'];
+
+include("../SaeMysql.php");
+//session_set_cookie_params(7200*12*7,'/','nuaakx.com');
+session_start();
+if(!empty($_SESSION["stId"]))
+{
+	$mysql=new SaeMysql();
+	$data=$mysql->getData("SELECT `email` FROM `wx_user` WHERE `number`='".$_SESSION["stId"]."'");
+	$name=$mysql->getLine("SELECT `name` FROM `wx_user` WHERE `number`='".$_SESSION["stId"]."'");
+	//$json=array();
+	if(!empty($data))
+	{
+		$json["error"]=0;
+		$json["email"]=$data["email"];
+		$json["name"]=$name["name"];
+	}
+	else
+	{
+		$json["error"]=1;
+	}
+
+}
+?>
+
+</body>
+</html>
+
+
+
---
+++ peixun/bxywelcome.php
@@ -0,0 +1,9 @@
+<html>
+<meta charset="utf-8">
+<body>
+
+Welcome <?php echo json_encode($json["email"]); ?><br>
+Your email address is: <?php echo json_encode($json["name"]); ?>
+
+</body>
+</html>
