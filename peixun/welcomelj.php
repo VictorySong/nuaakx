80daf95218fa6cf365f14703a8eb814e8c4c15ae
BXY <451812979@qq.com>
2018-05-28 01:28:04 +0800

    3

---
+++ peixun/welcomelj.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+
+<form action="bxywelcome.php" method="post">
+学号：<input type="number" name="Id"><br>
+<input type="submit">
+</form>
+
+//实现搜索功能。通过网页搜索，输入学号，能在科协数据库查找名字等相关信息并显示出来，通过网页显示
+
+
+</body>
+</html>
+
+
+
