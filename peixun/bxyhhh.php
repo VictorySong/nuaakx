51ee99c88582a4d19da10b24a990dd0ea563efa7
BXY <451812979@qq.com>
2018-05-27 23:39:17 +0800

    2

--- peixun/bxyhhh.php
+++ peixun/bxyhhh.php
@@ -6,35 +6,52 @@
 <body>
 
 <form action="bxywelcome.php" method="post">
-学号：<input type="number" id="stId"><br>
+学号：<input type="number" name="Id"><br>
 <input type="submit">
 </form>
-	
-<?php//实现搜索功能。通过网页搜索，输入学号，能在科协数据库查找名字等相关信息并显示出来，通过网页显示
-$json=array(); 
-$_SESSION["stId"]=document.getElementById("stId").value;
 
-include("../SaeMysql.php");
-//session_set_cookie_params(7200*12*7,'/','nuaakx.com');
-session_start();
-if(!empty($_SESSION["stId"]))
-{
-	$mysql=new SaeMysql();
-	$data=$mysql->getData("SELECT `email` FROM `wx_user` WHERE `number`='".$_SESSION["stId"]."'");
-	$name=$mysql->getLine("SELECT `name` FROM `wx_user` WHERE `number`='".$_SESSION["stId"]."'");
-	//$json=array();
-	if(!empty($data))
-	{
-		$json["error"]=0;
-		$json["email"]=$data["email"];
-		$json["name"]=$name["name"];
-	}
-	else
-	{
-		$json["error"]=1;
-	}
+//实现搜索功能。通过网页搜索，输入学号，能在科协数据库查找名字等相关信息并显示出来，通过网页显示
+$submit(function(e){
+		e.preventDefault();
+		var id=$("input[name=Id]").val();
+		console.log(id);
+		if(id!=undefined && id!=null)
+		{
+			$.post("bxy1.php",{stId:id}).done(function(data){
+				console.log(data);
+				try{
+					var da=JSON.parse(data);
+				}catch(e){ 
+					console.log(e);
+					return;
+				}
+			});
+		}
+	});
+					
+	
+});
+function userinfget(){
+	//获取用户个人信息
+	$.get("userinf.php").done(function(data){
+		console.log(data);
+		try{
+			var da=JSON.parse(data);
+		}
+		catch(e){
+			setTimeout(function(){document.location.href="../user/login.html"},1000);
+			setTimeout(function(){location.reload();},3000);//解决uc浏览器登陆后返回历史记录的问题页面不刷新的问题
+			return ;
+		}
+		
+		if(da["error"]==0)
+		{
+			window.inf=da["inf"];
+			$("#recruitsign [name=\"Id\"]").val(window.inf["stId"]);
+		}
+		
+	});
 }
-?>
 
 </body>
 </html>
