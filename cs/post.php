803fad6014c7015156e7ac7d77e5c5baffd99995
hyz <3042652167@qq.com>
2018-08-27 16:44:58 +0800

    a

--- cs/post.php
+++ cs/post.php
@@ -4,7 +4,6 @@ include("SaeMysql.php");
 if(!empty($_POST["number"]))
 {
 	$mysql=new SaeMysql();
-	$department=$_POST["department"];
 	if($mysql->runsql("INSERT INTO `cs1` (`stId`) VALUES ('".$_POST["number"]."'"))
 				die($mysql->errmsg());
 			
