6c51c0e8cdcd9ea56397dd85986830826ec125cd
hyz <3042652167@qq.com>
2018-09-03 22:15:54 +0800

    a

--- cs/get.php
+++ cs/get.php
@@ -2,7 +2,23 @@
 include("SaeMysql.php");
 
     $mysql=new SaeMysql();
-	$data=$mysql->getData("SELECT `num`,`vue` FROM `text` ");
+	//$data=$mysql->getData("SELECT `num`,`vue` FROM `text` ");
+	
+	
+	$sql="SELECT `num`,`vue` FROM `text`";
+	$result=mysql_query($sql,$mysql->ico);
+	$num=mysql_num_rows($result);
+	$Data=array();
+	for($i=0;$i<$num; $i++)
+	 {
+			$Data[$i]=mysql_fetch_assoc($result);
+		}
+	$data=array();
+	for($i=0;$i<25; $i++)
+	 {
+			$data[$i]=$Data[$num-25+$i];
+		}
+	
 	if(!empty($data))
 	{
 		$json["error"]=0;
