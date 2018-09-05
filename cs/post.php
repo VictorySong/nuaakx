04580347b8f4a182c4c59f8aa6df4002f16cd6af
shzhh <shz2261@qq.com>
2018-09-05 21:28:21 +0800

    dfsfsd

--- cs/post.php
+++ cs/post.php
@@ -5,7 +5,7 @@ if(!empty($_POST["num"]) && !empty($_POST["vue"]))
 {
 	$mysql=new SaeMysql();
 	if($mysql->runsql("INSERT INTO `text` (`num`,`vue`) VALUES ('".$_POST["num"]."','".$_POST["vue"]."') "))
-				die($mysql->errmsg());
+				//die($mysql->errmsg());
 			
 	$json["error"]=0;
 	echo json_encode($json);
