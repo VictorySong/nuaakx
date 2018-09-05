d84a7f1bfac9bedc57150c37a6db88d76a51dfb2
shzhh <shz2261@qq.com>
2018-09-05 20:59:12 +0800

    sdfds

--- cs/post.php
+++ cs/post.php
@@ -1,7 +1,7 @@
 <?php
 include("SaeMysql.php");
 
-//if(!empty($_POST["num"]) && !empty($_POST["vue"]))
+if(!empty($_POST["num"]) && !empty($_POST["vue"]))
 {
 	$mysql=new SaeMysql();
 	if($mysql->runsql("INSERT INTO `text` (`num`,`vue`) VALUES ('".$_POST["num"]."','".$_POST["vue"]."') "))
