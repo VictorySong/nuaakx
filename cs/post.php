bb12b0f623ed76e79304b49d901ab6711804b998
shzhh <shz2261@qq.com>
2018-09-16 10:32:17 +0800

    sfdsdf

--- cs/post.php
+++ cs/post.php
@@ -7,8 +7,10 @@ header('Access-Control-Allow-Headers:x-requested-with,content-type');
 
 $rws_post = $GLOBALS['HTTP_RAW_POST_DATA'];
 $mypost = json_decode($rws_post);
-$num = (string)$mypost->num;
-$vue = (string)$mypost->vue;
+/*$num = (string)$mypost->num;
+$vue = (string)$mypost->vue;*/
+$num = 1200;
+$vue = 111;
 
 //receive http json end
 if(!empty(num) && !empty(vue))
