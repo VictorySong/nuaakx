763a8a8b2af2d5b31879f6520bd2f5e937afd4a8
shzhh <shz2261@qq.com>
2018-09-16 10:29:53 +0800

    sfdsdf

--- cs/post.php
+++ cs/post.php
@@ -1,6 +1,29 @@
 <?php
 include("SaeMysql.php");
 
+//receive http json begin
+header("Access-Control-Allow-Origin:*"); 
+header('Access-Control-Allow-Headers:x-requested-with,content-type'); 
+
+$rws_post = $GLOBALS['HTTP_RAW_POST_DATA'];
+$mypost = json_decode($rws_post);
+$num = (string)$mypost->num;
+$vue = (string)$mypost->vue;
+
+//receive http json end
+if(!empty(num) && !empty(vue))
+{
+	$mysql=new SaeMysql();
+	if($mysql->runsql("INSERT INTO `text` (`num`,`vue`) VALUES ('".num."','".vue."') "))
+				//die($mysql->errmsg());
+			
+	$json["error"]=0;
+	echo json_encode($json);	
+}
+
+
+
+/*
 if(!empty($_POST["num"]) && !empty($_POST["vue"]))
 {
 	$mysql=new SaeMysql();
@@ -10,6 +33,8 @@ if(!empty($_POST["num"]) && !empty($_POST["vue"]))
 	$json["error"]=0;
 	echo json_encode($json);	
 }
+*/
+
 
 /*
 if(!empty($_POST["data"]))
