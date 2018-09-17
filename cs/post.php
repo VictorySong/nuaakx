c9dd8038a40c947ae6feaad4d0e2b2f157913378
shzhh <shz2261@qq.com>
2018-09-17 20:14:06 +0800

    sfdsdf

--- cs/post.php
+++ cs/post.php
@@ -12,10 +12,8 @@ echo json_encode($mypost);
 $mysql=new SaeMysql();
 //[{"num":"2583","vue":"78"},{"num":"2583","vue":"71"},...]
 for($i=0;$i<75;$i++){
-/*$num = (string)$mypost[$i]['num'];
-$vue = (string)$mypost[$i]['vue'];*/
-$num = 2500;
-$vue = 99;
+$num = (string)$mypost[$i]['num'];
+$vue = (string)$mypost[$i]['vue'];
 
 //receive http json end
 if(!empty($num) && !empty($vue))
