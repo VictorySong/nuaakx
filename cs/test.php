91cfa02f5e9124bf9160916e4df4bb596f815cf9
shzhh <shz2261@qq.com>
2018-09-23 21:03:16 +0800

    sfdsdf

--- cs/test.php
+++ cs/test.php
@@ -3,8 +3,9 @@ require_once('phpaes-master/src/Aes.php');
 use PhpAes\Aes;
 
 $aes = new Aes('abcdefgh01234567', 'ECB');
+$in[16] = "Hello, World!";
 
-$y = $aes->encrypt('hello world!');
+$y = $aes->encrypt($in);
 $x = $aes->decrypt($y);
 
 //echo base64_encode($y);
