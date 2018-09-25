ae2edad92be2e31c84f57873bc1c1691a2d1bfb5
shzhh <shz2261@qq.com>
2018-09-25 20:06:14 +0800

    sfdsdf

--- cs/test.php
+++ cs/test.php
@@ -2,8 +2,8 @@
 require_once('phpaes-master/src/Aes.php');
 use PhpAes\Aes;
 
-$in = "aaaabbbbccccdddd";
-$key = "abcdefgh01234234234234234242342432567";
+$in = "aaaabb34234243324324234234234234bbccccdddd";
+$key = "abcdefgh01234567";
 $aes = new Aes($key, 'ECB');
 
 $y = $aes->encrypt($in);
