e3fe3e19eb771339fb11a7278e42dc804d791b18
shzhh <shz2261@qq.com>
2018-09-26 15:43:42 +0800

    sfdsdf

--- cs/js/Wbeat.js
+++ cs/js/Wbeat.js
@@ -14,7 +14,7 @@ var myChart = new Chart(ctx, {
         datasets : [{	
 			label: '心电图',
 			backgroundColor: [
-               'rgba(255, 99, 132, 0)',             
+               'rgba(255, 99, 132, 0.4)',             
             ],
 			borderColor:[
 				'rgba(255, 99, 132, 0.6)',
