7fa6add2402cc222730624c8163ffe6a4be6b7e6
shzhh <shz2261@qq.com>
2018-09-27 21:47:45 +0800

    sfdsdf

--- cs/test.php
+++ cs/test.php
@@ -3,7 +3,7 @@ require_once('phpaes-master/src/Aes.php');
 use PhpAes\Aes;
 
 //$in = "0D0AE143183F0D553F39D2CAF8E83F3B";
-$in = "[{\"num\":\"4095\",\"";
+$in = "[{"num":"4095","";
 $key = "abcdefgh01234567";
 $aes = new Aes($key, 'ECB');
 
