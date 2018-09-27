b1bd2e69e31d14d69f063b5cb47f0834db860327
shzhh <shz2261@qq.com>
2018-09-27 20:48:30 +0800

    sfdsdf

--- cs/test.php
+++ cs/test.php
@@ -2,16 +2,17 @@
 require_once('phpaes-master/src/Aes.php');
 use PhpAes\Aes;
 
-$in = "0D0AE143183F0D553F39D2CAF8E83F3B";
+//$in = "0D0AE143183F0D553F39D2CAF8E83F3B";
+$in = "{\"num\":\"3501\",\"v";
 $key = "abcdefgh01234567";
 $aes = new Aes($key, 'ECB');
 
-$y = hex2bin($in);
-//$y = $aes->encrypt($in);
+//$y = hex2bin($in);
+$y = $aes->encrypt($in);
 $x = $aes->decrypt($y);
 
 //echo base64_encode($y);
-//echo bin2hex($y);
+echo bin2hex($y);
 echo $x;
 
 ?>
